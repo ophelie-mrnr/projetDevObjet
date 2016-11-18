a93591966c7fc912d0e34e78fac7dd25aa5a7139
ophelie-mrnr <ophelie.marinier@etu.univ-tours.fr>
2016-11-18 16:11:05 +0100

    Intégration graphe (avec eurreurs)

--- src/Interface/GrapheVisualisationVentes.java
+++ src/Interface/GrapheVisualisationVentes.java
@@ -1,9 +1,12 @@
 package Interface;
 
+import java.awt.Dimension;
 import java.util.Arrays;
 import java.util.Map;
+import java.util.Random;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -13,31 +16,32 @@ import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.CategoryItemRenderer;
 import org.jfree.chart.renderer.category.DefaultCategoryItemRenderer;
 import org.jfree.data.statistics.DefaultMultiValueCategoryDataset;
-import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.data.time.Day;
 
 public class GrapheVisualisationVentes {
 
-	ChartPanel chartPanel ; 
-	TimeSeriesCollection timeSeriesCollection;
-	JFreeChart jFreeChart;
+	static Random random = new Random();
+	
+	 ChartPanel cp ; 
+	 TimeSeriesCollection dataset;
+	 JFreeChart jFreeChart;
 	
 
 	public  GrapheVisualisationVentes (String charTitle){
-		timeSeriesCollection= new TimeSeriesCollection();
-
-		jFreeChart = createTimeChart(timeSeriesCollection, charTitle);
-		chartPanel = new ChartPanel(jFreeChart);
-
+		
+		dataset=  new TimeSeriesCollection();
+		jFreeChart = createTimeChart(dataset, charTitle);
+		cp = new ChartPanel(jFreeChart);
 	}
 
 	public void addNewSeries(String title){
-		timeSeriesCollection.addSeries(createTimeSeries(title));
+		dataset.addSeries(createTimeSeries(title));
 	}
 
 	public void clean(){
-		timeSeriesCollection.removeAllSeries();
+		dataset.removeAllSeries();
 	}
 
 	public static TimeSeries createTimeSeries(String title){
@@ -45,7 +49,8 @@ public class GrapheVisualisationVentes {
 				title = "Ventes";
 			TimeSeries timeSeries = new TimeSeries(title);
 
-			TimeSeriesCollection dataset;
+			for(int i = 0; i < randomValue(10, 30); i++)
+				timeSeries.addOrUpdate(new Day(randomValue(1,28), randomValue(1, 12), randomValue(2011, 2014)), randomValue(1, 999));
 			
 			/*Pour les données du jfreeChart (jFreeChart) de visualisation des ventes :
 				De date de début à date de fin :
@@ -58,43 +63,49 @@ public class GrapheVisualisationVentes {
 
 				   Pour une date on aura donc 1 total de ventes
 				 On incrément la date pour faire la meme chose à la date suivante. jusqu’a ce qu’on arrive à la date de fin.
-			 */
-			//dataset.addSeries(timeSeries);
-			return timeSeries ;
+			 */						
+			
+			return timeSeries ;	
 		}
-
-protected ChartPanel createChart(Map<String, Map<Integer, Integer>> data) {
-	// création du graphique vide
-	JFreeChart chart = ChartFactory.createLineChart("Titre", "x", "y", null, PlotOrientation.VERTICAL, true, false, false);
- 
-	// insertion des données
-	int index = 0; // identifie les différentes courbes
-	for (String curveTitle : data.keySet()) {
-		// on récupère les données d'une courbe
-		DefaultMultiValueCategoryDataset dataset = new DefaultMultiValueCategoryDataset();
-		Map<Integer, Integer> instanceData = data.get(curveTitle);
-		for (Map.Entry<Integer, Integer> entry : instanceData.entrySet()) {
-			// insertion du point, notez la nécessité de
-			// faire une liste, ce qui n'est pas naturel
-			int x = entry.getKey();
-			int y = entry.getValue();
-			dataset.add(Arrays.asList(y), curveTitle, x);
-		}
- 
-		// on insère les données dans le graphique
-		CategoryPlot plot = (CategoryPlot) chart.getPlot();
-		plot.setDataset(index, dataset);
- 
-		// création d'un renderer pour chaque courbe
-		// sinon seule la première est affichée
-		CategoryItemRenderer renderer = new DefaultCategoryItemRenderer();
-		plot.setRenderer(index, renderer);
- 
-		index++;
-	}
-	return new ChartPanel(chart);
-}
 	
+	protected ChartPanel createChart(Map<String, Map<Integer, Integer>> data) {
+		// création du graphique vide
+		JFreeChart chart = ChartFactory.createLineChart("Titre", "x", "y", null, PlotOrientation.VERTICAL, true, false, false);
+	 
+		// insertion des données
+		int index = 0; // identifie les différentes courbes
+		for (String curveTitle : data.keySet()) {
+			// on récupère les données d'une courbe
+			DefaultMultiValueCategoryDataset dataset = new DefaultMultiValueCategoryDataset();
+			Map<Integer, Integer> instanceData = data.get(curveTitle);
+			for (Map.Entry<Integer, Integer> entry : instanceData.entrySet()) {
+				// insertion du point, notez la nécessité de
+				// faire une liste, ce qui n'est pas naturel
+				int x = entry.getKey();
+				int y = entry.getValue();
+				dataset.add(Arrays.asList(y), curveTitle, x);
+			}
+	 
+			// on insère les données dans le graphique
+			CategoryPlot plot = (CategoryPlot) chart.getPlot();
+			plot.setDataset(index, dataset);
+	 
+			// création d'un renderer pour chaque courbe
+			// sinon seule la première est affichée
+			CategoryItemRenderer renderer = new DefaultCategoryItemRenderer();
+			plot.setRenderer(index, renderer);
+	 
+			index++;
+		}
+		return new ChartPanel(chart);
+	}
+	
+	
+	private static int randomValue(int min, int max){
+		int randV = random.nextInt(max - min +1)+min;
+		return (randV > 0) ? randV : 1; 
+	}
+	 
 
 	public static JFreeChart createTimeChart(TimeSeriesCollection dataset, String title){
 		JFreeChart chart = ChartFactory.createTimeSeriesChart(
@@ -104,13 +115,5 @@ protected ChartPanel createChart(Map<String, Map<Integer, Integer>> data) {
 				dataset);
 
 		return chart;
-
-	}
-
-	private static int remplissageValeurs(){
-		/*int valeur = ;
-		return valeur;
-				*/
-		return 0;
-	}
+	}	
 }
--- src/Interface/VisualisationDesVentes.java
+++ src/Interface/VisualisationDesVentes.java
@@ -1,173 +1,195 @@
 package Interface;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.io.File;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.filechooser.FileNameExtensionFilter;
 
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.time.Day;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.ChartFactory;
 
-import DAO.*;
-
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import DAO.MaConnexion;
 
 public class VisualisationDesVentes extends javax.swing.JPanel implements ActionListener{
 
 	// Variables declaration                     
-	private javax.swing.JTextField DateDebutTextField;
-	private javax.swing.JTextField DateFinTextField;
-	private javax.swing.JButton validerButton;
-	private javax.swing.JComboBox jComboBox1;
-	private javax.swing.JLabel jLabel1;
-	private javax.swing.JLabel jLabel2;
-	private javax.swing.JLabel jLabel3;
-	private javax.swing.JPanel jPanel1;
-	private TimeSeriePanel tsp; 
+    private javax.swing.JTextField DateDebutTextField;
+    private javax.swing.JTextField DateFinTextField;
+    private javax.swing.JButton validerButton;
+    private javax.swing.JComboBox jComboBox1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1; 
+    private String[] tab;
+    private javax.swing.JButton enregistrer;
+    
+    // variables pour le graphe
+    private javax.swing.JPanel jPanelGraphe;
+    private javax.swing.JPanel actionPanel; 
+   	private javax.swing.JPanel titlePanel; 
+   	private javax.swing.JPanel buttonPanel; 
+   	private ChartPanel chartPanel;
+   	private GrapheVisualisationVentes tsp; 
+    
+    // End of variables declaration  
+    
+    /**
+     * Creates new form NewJPanel
+     */
+    public VisualisationDesVentes() {
+        initComponents();
+        initGrapheVisualisationVentes();
+    }
 
-	private ChartPanel chartPanel;
+    
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        DateDebutTextField = new javax.swing.JTextField();
+        DateFinTextField = new javax.swing.JTextField();
+        jComboBox1 = new javax.swing.JComboBox();
+        jPanel1 = new javax.swing.JPanel();
+        validerButton = new javax.swing.JButton();
+        jPanelGraphe = new javax.swing.JPanel();
+        
+
+        jLabel1.setText("Pays");
+
+        jLabel2.setText("Date de début");
+
+        jLabel3.setText("Date de fin");
+
+        DateDebutTextField.setFont(new java.awt.Font("Tahoma", 2, 11)); // NOI18N
+        DateDebutTextField.setText("Rentrez date de début");
+
+        DateFinTextField.setFont(new java.awt.Font("Tahoma", 2, 11)); // NOI18N
+        DateFinTextField.setText("Rentrez date de fin");
+        DateFinTextField.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DateFinTextFieldActionPerformed(evt);
+            }
+        });
+
+        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 738, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 401, Short.MAX_VALUE)
+        );
+
+        validerButton.setText("Valider");
+        validerButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            	actionPerformed(evt);
+            }
+        });
 
 
-	private String[] tab;
-
-	// variables pour le graphe
-	private javax.swing.JPanel jPanelGraphe;
-
-	// End of variables declaration  
-
-	/**
-	 * Creates new form NewJPanel
-	 */
-	public VisualisationDesVentes() {
-		initComponents();
-	}
-
-
-	@SuppressWarnings("unchecked")
-	private void initComponents() {
-
-		jLabel1 = new javax.swing.JLabel();
-		jLabel2 = new javax.swing.JLabel();
-		jLabel3 = new javax.swing.JLabel();
-		DateDebutTextField = new javax.swing.JTextField();
-		DateFinTextField = new javax.swing.JTextField();
-		jComboBox1 = new javax.swing.JComboBox();
-		jPanel1 = new javax.swing.JPanel();
-		validerButton = new javax.swing.JButton();
-		jPanelGraphe = new javax.swing.JPanel();
-
-
-		tsp = new TimeSeriePanel("Sales :");
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(jLabel1)
+                        .addGap(30, 30, 30)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(49, 49, 49)
+                        .addComponent(jLabel2)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(DateDebutTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(DateFinTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(33, 33, 33)
+                        .addComponent(validerButton))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(45, 45, 45)
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(319, 319, 319)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3)
+                    .addComponent(DateDebutTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(DateFinTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(validerButton))
+                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+    }// </editor-fold>    
+    
+    private void initGrapheVisualisationVentes() { 
+    	/*GrapheVisualisationVentes testGraphe = new GrapheVisualisationVentes("nouveau graphe"); 
+        ChartPanel graphic = testGraphe.getChartPanel(); 
+       
+        //jPanelGraphe.setLayout(new java.awt.BorderLayout()); 
+        //jPanelGraphe.add(graphic1, BorderLayout.CENTER); 
+      
+        graphic.validate();
+    	jPanelGraphe.add(graphic);  	
+    	 
+      	jPanelGraphe.setVisible(true);
+      	graphic.setVisible(true);*/
+    	
+    	tsp = new GrapheVisualisationVentes("Ventes :");
 		chartPanel = tsp.cp;
+		
+		jPanelGraphe.add(BorderLayout.CENTER, chartPanel);
+		jPanelGraphe.add(BorderLayout.SOUTH, actionPanel);
+    }    
+        
 
-		jLabel1.setText("Pays");
-
-		jLabel2.setText("Date de dï¿½but");
-
-		jLabel3.setText("Date de fin");
-
-		DateDebutTextField.setFont(new java.awt.Font("Tahoma", 2, 11)); // NOI18N
-		DateDebutTextField.setText("Rentrez date de dï¿½but");
-
-		DateFinTextField.setFont(new java.awt.Font("Tahoma", 2, 11)); // NOI18N
-		DateFinTextField.setText("Rentrez date de fin");
-		DateFinTextField.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				DateFinTextFieldActionPerformed(evt);
-			}
-		});
-
-		jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(creationComboBox()));
-
-		javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-		jPanel1.setLayout(jPanel1Layout);
-		jPanel1Layout.setHorizontalGroup(
-				jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGap(0, 738, Short.MAX_VALUE)
-				);
-		jPanel1Layout.setVerticalGroup(
-				jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGap(0, 401, Short.MAX_VALUE)
-				);
-
-		validerButton.setText("Valider");
-		validerButton.addActionListener(new java.awt.event.ActionListener() {
-			public void actionPerformed(java.awt.event.ActionEvent evt) {
-				ValiderActionPerformed(evt);
-			}
-		});
-
-
-		javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
-		this.setLayout(layout);
-		layout.setHorizontalGroup(
-				layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-						.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-								.addGroup(layout.createSequentialGroup()
-										.addGap(22, 22, 22)
-										.addComponent(jLabel1)
-										.addGap(30, 30, 30)
-										.addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-										.addGap(49, 49, 49)
-										.addComponent(jLabel2)
-										.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-										.addComponent(DateDebutTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
-										.addGap(18, 18, 18)
-										.addComponent(jLabel3)
-										.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-										.addComponent(DateFinTextField, javax.swing.GroupLayout.PREFERRED_SIZE, 125, javax.swing.GroupLayout.PREFERRED_SIZE)
-										.addGap(33, 33, 33)
-										.addComponent(validerButton))
-								.addGroup(layout.createSequentialGroup()
-										.addGap(45, 45, 45)
-										.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-								.addGroup(layout.createSequentialGroup()
-										.addGap(319, 319, 319)))
-						.addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-				);
-		layout.setVerticalGroup(
-				layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-				.addGroup(layout.createSequentialGroup()
-						.addGap(40, 40, 40)
-						.addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-								.addComponent(jLabel1)
-								.addComponent(jLabel2)
-								.addComponent(jLabel3)
-								.addComponent(DateDebutTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addComponent(DateFinTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-								.addComponent(validerButton))
-						.addGap(18, 18, 18)
-						.addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-						.addContainerGap())
-				);
-	}// </editor-fold>                        
-
-	private void DateFinTextFieldActionPerformed(java.awt.event.ActionEvent evt) {                                                 
-
-		// TODO add your handling code here:
+    private void DateFinTextFieldActionPerformed(java.awt.event.ActionEvent evt) {                                                 
+       
+    }                                                             
+    
+	
+	public void actionPerformed(ActionEvent evt) {
+		// A FAIRE   	
+    	/*
+    	 * if (Country) 
+    	 */   	
+    	//jPanelGraphe.setVisible(true);
+    	jPanelGraphe.validate(); 
+    	System.out.println("clique sur bouton validerButton.");
+		
 	}  
 	
-	
 
-	private void ValiderActionPerformed(java.awt.event.ActionEvent evt) {                                        
+
+
+
+private void ValiderActionPerformed(java.awt.event.ActionEvent evt) {                                        
 		// TODO add your handling code here:
 		JButton enregistrer = new JButton("Enregistrer"); 
 		jPanel1.setSize(700,700);
@@ -246,14 +268,7 @@ public class VisualisationDesVentes extends javax.swing.JPanel implements Action
 	}
 
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
 
-
-
-
-
+	
+	
 }
